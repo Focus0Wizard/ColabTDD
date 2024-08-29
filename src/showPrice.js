682c9724c934379a96e6c64f52ed00d1c2cb95ff
Diego Trigo <trigobol23@gmail.com>
2024-08-29 10:02:19 -0400

    add showNetCost test failed

--- src/showPrice.js
+++ src/showPrice.js
@@ -1,6 +1,15 @@
+import showTotal from "./showTotalItems.js"
+
 function showPrice(total){
 
     return total
 }
 
-export default showPrice
+function showNetCost(total, items){
+    items = showTotal()
+    //return total*items
+    return 2048
+}
+
+
+export {showPrice, showNetCost};
--- src/showPrice.spec.js
+++ src/showPrice.spec.js
@@ -1,7 +1,10 @@
-import showPrice from "./showPrice.js";
+import {showPrice, showNetCost } from "./showPrice.js";
 
 describe("Mostrar monto total", () => {
     it("No muestra el monto total", () => {
         expect(showPrice(100)).toEqual(100);
+    });
+    it("No muestra el precio neto", () => {
+        expect(showNetCost(100, 2)).toEqual(200);
     });
 });
